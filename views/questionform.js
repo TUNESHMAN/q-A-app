fa51693bcea19682df815b331fcf48489a0b71eb
Babatunde <babatundea15@gmail.com>
2020-06-23 12:28:10 +0100

    setup firebase config

---
+++ views/questionform.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB6vaKlANCatJJGVPjFb6V89grgbI7Uc2o",
+    authDomain: "questionform-ed5f5.firebaseapp.com",
+    databaseURL: "https://questionform-ed5f5.firebaseio.com",
+    projectId: "questionform-ed5f5",
+    storageBucket: "questionform-ed5f5.appspot.com",
+    messagingSenderId: "384186672300",
+    appId: "1:384186672300:web:c098bd5a0a3fead37f2b09"
+  };
